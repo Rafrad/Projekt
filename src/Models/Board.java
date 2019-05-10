f040b2290e72c4fcbd97318ad28ea7d38beda5bf
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2019-05-11 00:25:43 +0200

    full board

--- src/Models/Board.java
+++ src/Models/Board.java
@@ -3,14 +3,14 @@ package Models;
 import Exceptions.PlayerColorException;
 import Models.Pieces.*;
 import javafx.util.Pair;
+
 import java.util.List;
 
 public class Board {
-    //TODO: delete publics
     public Piece[][] board;
     public Piece[][] boardOfPossibleMoves;
 
-    public Board() throws PlayerColorException  {
+    public Board() throws PlayerColorException {
         board = new Piece[8][8];
         boardOfPossibleMoves = new Piece[8][8];
 
@@ -28,24 +28,17 @@ public class Board {
                             break;
                         case 1:
                         case 6:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteKnight();
+                            board[i][j] = new Knight(true);
                             break;
                         case 2:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new Bishop("black");
                         case 5:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new Bishop("white");
-
+                            board[i][j] = new Bishop(true);
                             break;
                         case 3:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteQueen();
+                            board[i][j] = new Queen(true);
                             break;
                         case 4:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteKing();
+                            board[i][j] = new WhiteKing();
                             break;
                     }
                 } else if (i == 0) {
@@ -56,28 +49,20 @@ public class Board {
                             break;
                         case 1:
                         case 6:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteKnight();
+                            board[i][j] = new Knight(false);
                             break;
                         case 2:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new Bishop("black");
                         case 5:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new Bishop("white");
-
+                            board[i][j] = new Bishop(false);
                             break;
                         case 3:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteQueen();
+                            board[i][j] = new Queen(false);
                             break;
                         case 4:
-                            board[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteKing();
+                            board[i][j] = new BlackKing();
                             break;
                     }
-                }
-                else if (i == 6) {
+                } else if (i == 6) {
                     board[i][j] = new WhitePawn();
                 } else if (i == 1) {
                     board[i][j] = new BlackPawn();
@@ -94,48 +79,11 @@ public class Board {
          * movable board init
          */
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (i == 0 || i == 7) {
-                    switch (j) {
-                        case 0:
-                            boardOfPossibleMoves[i][j] = new Rook(false);
-                            break;
-                        case 7:
-                            boardOfPossibleMoves[i][j] = new Rook(true);
-                            break;
-                        case 1:
-                        case 6:
-                            boardOfPossibleMoves[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteKnight();
-                            break;
-                        case 2:
-                            boardOfPossibleMoves[i][j] = new EmptyTile();
-//                            board[i][j] = new Bishop("black");
-                        case 5:
-                            boardOfPossibleMoves[i][j] = new EmptyTile();
-//                            board[i][j] = new Bishop("white");
-
-                            break;
-                        case 3:
-                            boardOfPossibleMoves[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteQueen();
-                            break;
-                        case 4:
-                            boardOfPossibleMoves[i][j] = new EmptyTile();
-//                            board[i][j] = new WhiteKing();
-                            break;
-                    }
-                } else if (i == 6) {
-                    boardOfPossibleMoves[i][j] = new WhitePawn();
-                } else if (i == 1) {
-                    boardOfPossibleMoves[i][j] = new BlackPawn();
-                } else {
-                    boardOfPossibleMoves[i][j] = new EmptyTile();
-                }
-            }
-
+        for (int row = 0; row < 8; row++) {
+            System.arraycopy(board[row], 0, boardOfPossibleMoves[row], 0, 8);
         }
+
+
     }
 
 
@@ -151,14 +99,43 @@ public class Board {
                         System.out.print("b ");
                         break;
                     case "Rook":
-                       player =  ((Rook)board[i][j]).getPlayer();
-                       if(player) {
-                           System.out.print("R ");
-                       } else {
-                           System.out.print("r ");
-                       }
-                       break;
-
+                        player = ((Rook) board[i][j]).getPlayer();
+                        if (player) {
+                            System.out.print("R ");
+                        } else {
+                            System.out.print("r ");
+                        }
+                        break;
+                    case "Knight":
+                        player = ((Knight) board[i][j]).getPlayer();
+                        if (player) {
+                            System.out.print("K ");
+                        } else {
+                            System.out.print("k ");
+                        }
+                        break;
+                    case "Bishop":
+                        player = ((Bishop) board[i][j]).getPlayer();
+                        if (player) {
+                            System.out.print("B ");
+                        } else {
+                            System.out.print("b ");
+                        }
+                        break;
+                    case "Queen":
+                        player = ((Queen) board[i][j]).getPlayer();
+                        if (player) {
+                            System.out.print("Q ");
+                        } else {
+                            System.out.print("q ");
+                        }
+                        break;
+                    case "WhiteKing":
+                        System.out.print("Y ");
+                        break;
+                    case "BlackKing":
+                        System.out.print("y ");
+                        break;
                     default:
                         System.out.print("x ");
                         break;
@@ -175,7 +152,7 @@ public class Board {
     public void AddPossibleMoves(List<Pair<Integer, Integer>> list) {
         int row = 0;
         int column = 0;
-        for(int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             row = list.get(i).getKey();
             column = list.get(i).getValue();
 
@@ -186,22 +163,43 @@ public class Board {
     public void PrintMovableBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-
-                if(boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("WhitePawn")) {
+//chyba swich h3h3h
+                if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("WhitePawn")) {
                     System.out.print("w ");
-                } else if(boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("BlackPawn")) {
+                } else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("BlackPawn")) {
                     System.out.print("b ");
-                } else if(boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Rook")) {
-                    if(((Rook)boardOfPossibleMoves[i][j]).getPlayer()) {
+                } else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Rook")) {
+                    if (((Rook) boardOfPossibleMoves[i][j]).getPlayer()) {
                         System.out.print("R ");
                     } else {
                         System.out.print("r ");
                     }
+                } else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Knight")) {
+                    if (((Knight) boardOfPossibleMoves[i][j]).getPlayer()) {
+                        System.out.print("K ");
+                    } else {
+                        System.out.print("k ");
+                    }
+                } else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Bishop")) {
+                    if (((Bishop) boardOfPossibleMoves[i][j]).getPlayer()) {
+                        System.out.print("B ");
+                    } else {
+                        System.out.print("b ");
+                    }
+                }else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("BlackKing")) {
+                        System.out.print("y ");
+                }else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("WhiteKing")) {
+                    System.out.print("Y ");
+                } else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Queen")) {
+                    if (((Queen) boardOfPossibleMoves[i][j]).getPlayer()) {
+                        System.out.print("Q ");
+                    } else {
+                        System.out.print("q ");
+                    }
                 }
-                else if(boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Mark_MovableTile")) {
+                else if (boardOfPossibleMoves[i][j].getClass().getSimpleName().equals("Mark_MovableTile")) {
                     System.out.print("m ");
-                }
-                else {
+                } else {
                     System.out.print("x ");
                 }
             }
@@ -210,7 +208,7 @@ public class Board {
     }
 
     public void ClearPossibleMoves() {
-        for(int row = 0; row <8; row ++) {
+        for (int row = 0; row < 8; row++) {
             System.arraycopy(board[row], 0, boardOfPossibleMoves[row], 0, 8);
         }
     }
