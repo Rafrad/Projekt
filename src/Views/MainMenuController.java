6464deae01bc24e911565d7b14a355c6af5c0a2e
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2019-05-15 01:44:59 +0200

    Main Menu updated

--- src/Views/MainMenuController.java
+++ src/Views/MainMenuController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -14,36 +13,30 @@ import java.io.IOException;
 
 
 public class MainMenuController {
-    @FXML
-    Button start;
-
-    @FXML
-    private AnchorPane rootLayout;
-
-    @FXML
-    public void loadSecond() throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("Views/Game.fxml"));
-        rootLayout.getChildren().setAll(pane);
-    }
+    @FXML Button startButton;
+    @FXML Button exitButton;
 
     /*
-     * When this method is called, it will change the Scene to
-     * a Game
+     * When this method is called, it will change the Scene to a Game
      */
 
     public void startGame(ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("/Views/Game.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
 
-        //This line gets the Stage information
+
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setWidth(1400);
         window.setHeight(800);
         window.setScene(tableViewScene);
-//        window.setResizable(false);
 
         window.show();
 
+    }
+
+    public void exitGame() {
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
