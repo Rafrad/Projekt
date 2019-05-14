abfca8b689771e49b2cbc4ae8fec5007215af0ec
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2019-05-15 01:02:55 +0200

    Exceptions updated

--- src/Exceptions/PlayerColorException.java
+++ src/Exceptions/PlayerColorException.java
@@ -1,5 +1,12 @@
 package Exceptions;
 
+/*
+
+ * Made for development & tests
+ * Some pieces cannot be initialized without given color
+
+ */
+
 public class PlayerColorException extends Exception {
     public PlayerColorException(String message) {
         super(message);
--- src/Exceptions/WrongBoardException.java
+++ src/Exceptions/WrongBoardException.java
@@ -1,5 +1,12 @@
 package Exceptions;
 
+/*
+
+ * Made for development & tests
+ * Board must be given by Game class
+
+ */
+
 public class WrongBoardException extends Exception {
     public WrongBoardException(String message) {
         super(message);
