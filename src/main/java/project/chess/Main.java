090a08b2512c745f2cf87e05ce7dec8dd7f6e5e6
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2019-06-01 00:33:32 +0200

    main

--- src/main/java/project/chess/Main.java
+++ src/main/java/project/chess/Main.java
@@ -7,12 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/*
- * Currently I don't know how to make separate
- * folder for Controllers
- *
- * At the moment main.java.project.chess.Controllers & Controllers are in the same folder
- */
 
 public class Main extends Application {
 
