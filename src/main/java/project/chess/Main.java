a3ce0006c4fb4da2fc43b3c542139289700def0c
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2019-06-06 22:10:28 +0200

    main done

--- src/main/java/project/chess/Main.java
+++ src/main/java/project/chess/Main.java
@@ -4,21 +4,68 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * Main class of application.
+ */
 
 public class Main extends Application {
+
+    /**
+     * Starts application.
+     *
+     * @param primaryStage application's first stage
+     * @throws IOException thrown when loader cannot find MainMenu.fxml view
+     */
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Views/fxml/MainMenu.fxml"));
         primaryStage.setTitle("Chess");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image("/Images/icon.png"));
+
+
+        primaryStage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::closeWindowEvent);
     }
 
+    /**
+     * This method doesn't allow to close application by accident
+     *
+     * @param event window event
+     */
 
+    private void closeWindowEvent(WindowEvent event) {
+        System.out.println("Window close request ...");
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getButtonTypes().remove(ButtonType.OK);
+        alert.getButtonTypes().add(ButtonType.CANCEL);
+        alert.getButtonTypes().add(ButtonType.YES);
+        alert.setTitle("Are you done?");
+        alert.setContentText("Do you wish to exit?");
+        Optional<ButtonType> res = alert.showAndWait();
+
+        if (res.isPresent()) {
+            if (res.get().equals(ButtonType.CANCEL)) {
+                event.consume();
+            } else {
+                System.exit(0);
+            }
+        }
+    }
+
+    
     public static void main(String[] args) {
         launch(args);
     }
